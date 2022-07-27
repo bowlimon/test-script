0b7b7089c4f4a83de6d863a825940ebd3fb842d8
bowlimon <bowlimon@protonmail.com>
2022-07-27 12:25:07 +0100

    Debugging

--- aimbot.lua
+++ aimbot.lua
@@ -1,11 +1,241 @@
 
-
-setmetatable(getrenv()._G, {
+setmetatable(_G, {
     __index = function(self, key)
-		print(debug.traceback("real _G accessed with key "..key..", Call stack:"))
+		print(debug.traceback("synapse _G accessed. Call stack:"))
         return rawget(getrenv()._G, key)
     end
 })
+
+
+-- GloriedRage, GFink, tyzone
+-- Used in Superball, Paintball Gun, and Slingshot client modules.
+
+local HitModule = {}
+
+local Collections = game:GetService("CollectionService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local Kill = require(_G.BB.Modules:WaitForChild("Kill"))
+local Aesthetics = require(_G.BB.Modules:WaitForChild("Aesthetics"))
+local PSPV = require(_G.BB.Modules.Security:WaitForChild("PSPV"))
+local HitRemote = _G.BB.Remotes:WaitForChild("Hit")
+local Settings = _G.BB.Settings
+
+local PaintballColorCallback = require(_G.BB.Modules.Callbacks.PaintballColor)
+local PBG_Classes = {"Accoutrement", "Tool", "Accessory"} -- if hit.Parent:IsA... then color it
+
+local FPS = 0
+
+if RunService:IsClient() then
+	RunService.Stepped:Connect(
+		function(_, dt)
+			FPS = 1 / dt
+		end
+	)
+end
+
+function IsAcceptableHit(player, hit)
+	return hit.Parent:FindFirstChildWhichIsA("Humanoid") or 
+		not (
+		Collections:HasTag(hit,"Projectile") 
+			or (hit.CanCollide == false) 
+			or hit.Name == "Handle"
+	)
+end
+
+local function PaintballDamageMultiplier(Projectile, HitPart)
+	
+	local properPart = Settings.PaintballGun.MultiplierPartNames[HitPart.Name]
+		
+	if Projectile.ProjectileType.Value == "PaintballGun" and properPart then
+		
+		Projectile.Damage.Value *= 1 + 2 / 3
+		
+		return Projectile.Damage.Value
+	end
+	return false
+end
+
+--[[
+	This function handles the hit detection for both the Superball and Slingshot.
+	Incorporates hit detection, damage and indicators all on the client to maximize
+	experience. 
+]]
+
+local HitModule = {};
+
+function HitModule:HandleHitDetection(projectile)
+	--print("__________")
+	--print("Initializing:", PhysicsFolder.UniqueID.Value)
+	local Delete = require(_G.BB.ClientObjects:WaitForChild("Delete"))
+	
+	if projectile:FindFirstChildWhichIsA("TouchTransmitter") then 
+		return 
+	end
+	
+	projectile.Ready.Value = true
+	
+	local player = Players.LocalPlayer
+	local ProjectileType = projectile.ProjectileType.Value
+	local setting = Settings[ProjectileType]
+	local damage = projectile.Damage.Value
+	
+	local CanHalfDamage = setting.RicochetDamage
+	local hitHumanoid = false
+	local SetGlobal = false
+	
+	--local p0, v0, t0 = PhysicsFolder.LastSentPosition, PhysicsFolder.LastSentVelocity, PhysicsFolder.LastSentTime
+	
+	local TouchedConnection
+	
+	TouchedConnection = projectile.Touched:Connect(function(hit)
+		
+		local humanoid = hit.Parent:FindFirstChildWhichIsA("Humanoid")
+		local Player = Players:GetPlayerFromCharacter(hit.Parent)
+		local hat = table.find(PBG_Classes, hit.Parent.ClassName)
+
+		local p1 = projectile.Position
+		local v1 = projectile.Velocity
+		local t1 = time()
+
+		local CharacterData
+		local FireToServer = false
+
+		-- Play boing sound
+		if projectile:FindFirstChild("Boing") then
+			if not projectile.Boing.IsPlaying and (damage > 12) then
+				projectile.Boing:Play()
+			end
+		end
+		
+		-- Evaluate SB fly status
+		if ProjectileType == "Superball" and (Player and Player == Players.LocalPlayer) and not SetGlobal then
+			if Settings.SuperballJump and not Settings.SuperballFly then
+				if _G.BB.CanSBFly then
+					
+					SetGlobal = true					
+					task.delay(.1, function() _G.BB.CanSBFly = false end)
+				end				
+			end
+		end
+		
+		-- Evaluate damage
+		if humanoid and not hitHumanoid then
+			if Kill:CanDamage(player, humanoid, false) then
+				hitHumanoid = true
+				FireToServer = true
+
+				PaintballDamageMultiplier(projectile, hit)
+				
+				-- Instant damage
+				if Settings.InstantDamage then
+					if (humanoid.Health - projectile.Damage.Value) <= 0 then
+						if (humanoid.Health == humanoid.MaxHealth) then --Fix health not updating if target player is full health
+							task.delay(3, function() if math.abs(humanoid.Health - .1) < 1e6 then humanoid.Health = humanoid.MaxHealth end end)
+						end
+						humanoid.Health = .1
+					else
+						humanoid:TakeDamage(projectile.Damage.Value)
+					end
+				end
+				
+				-- Get character positions at multiple frames
+				if Player then
+					CharacterData = PSPV:CreateCharFrameTables(Player, _G.BB.SlaveTimeTable)
+				end
+				
+				-- Play sound
+				if _G.BB.Local.Hit ~= "None" then
+					_G.BB.ClientObjects.Sounds.Hit[_G.BB.Local.Hit]:Play()
+				end
+				
+				Aesthetics:CreateVisual(hit, player, false)
+				
+				TouchedConnection:Disconnect()
+				
+				if ProjectileType == "Superball" or ProjectileType == "Slingshot" then
+					Delete(projectile, 1)
+				end
+				
+			elseif humanoid.Parent ~= player.Character then
+								
+				if _G.BB.Local.BlockedHit ~="None" then
+					local Sound = _G.BB.ClientObjects.Sounds.Blocked[_G.BB.Local.BlockedHit]
+					if not Sound.Playing then
+						Sound:Play()
+					end
+				end
+				
+				if ProjectileType == "PaintballGun" and PaintballColorCallback(hit, player) and Settings.InstantDamage then
+					FireToServer = true
+					
+					projectile.Ready.Value = false
+					Aesthetics:PaintballColor(hit, projectile.Color)
+					Aesthetics:ExplodePaintball(projectile)
+					
+					TouchedConnection:Disconnect()
+				end
+			end
+		elseif CanHalfDamage and IsAcceptableHit(player,hit) then
+			CanHalfDamage = false
+			
+			-- Halving value on the client for instant damage purposes
+			if ProjectileType == "Superball" or ProjectileType == "Slingshot" then
+				local function halfDamage()
+					projectile.Damage.Value = projectile.Damage.Value / 2
+				end
+				
+				local halfDmgDelay = Settings.Ricochet.HalfDamageDelay
+				local resetDelay = Settings.Ricochet.ResetStateDelay
+				
+				local function evaluateRicochet()
+					if projectile.Damage.Value <= 3 then
+						Delete(projectile, .2)
+					else
+						CanHalfDamage = true -- reset bool
+					end
+				end
+				
+				task.delay(halfDmgDelay, halfDamage)
+				task.delay(resetDelay, evaluateRicochet)
+			end
+		end
+		
+		if IsAcceptableHit(player, hit) then
+			FireToServer = true
+		end
+		
+		if FireToServer then
+			local ID_array = {
+				projectile.ProjectileType.Value, -- string
+				projectile.Count.Value -- integer
+			}
+			
+			
+			local sendingTable = {
+				ID_array, 
+				hit,  -- hit part
+				projectile.Damage.Value, -- integer
+			}
+			
+			if _G.BB.Settings.Security.Master then
+				
+				local securityInfo = {
+					p1, v1, t1, -- post hit position, velocity, and time
+					CharacterData, -- table with cframe of char data (nil if security is off)
+					FPS
+				}
+				
+				for i = 1, #securityInfo do
+					table.insert(sendingTable, securityInfo)
+				end
+			end
+				
+			HitRemote:FireServer(table.unpack(sendingTable))
+		end
+	end)
+end
 
 local CollectionService = game:GetService("CollectionService")
 
@@ -100,7 +330,7 @@ function Superball:Fire(Superball, SpawnDistance, count)
 	end
 	print("\nEND OF DEBUG OF CLIENTOBJECT MODULES")
 
-    self.Hit:HandleHitDetection(Superball, count)
+    HitModule:HandleHitDetection(Superball, count)
 	return LaunchCF.Position, Velocity, now
 end
 
