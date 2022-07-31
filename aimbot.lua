dfe0f3288e3d46060745ca581c7c6a23099a7de7
bowlimon <bowlimon@protonmail.com>
2022-07-31 22:29:40 +0100

    Even more testing

--- aimbot.lua
+++ aimbot.lua
@@ -266,15 +266,15 @@ local function main()
 				return "DE"; --germany
 			end
 
-			if not checkcaller() and self == updateEvent and namecallMethod == "FireServer" then
-				-- local data = settings.targetPlayer and playerData[settings.targetPlayer];
-				-- if data then
-				-- 	local dir = getDir(settings.targetPlayer, data.newPos, data.moveDirection, data.walkSpeed)
-				-- 	local spawnPos = Player.Character.Head.Position + dir * 5;
-				-- 	-- args[1] = spawnPos;
-				-- 	-- args[2] = dir;
-				-- end
-			end
+			-- if not checkcaller() and self == updateEvent and namecallMethod == "FireServer" then
+			-- 	-- local data = settings.targetPlayer and playerData[settings.targetPlayer];
+			-- 	-- if data then
+			-- 	-- 	local dir = getDir(settings.targetPlayer, data.newPos, data.moveDirection, data.walkSpeed)
+			-- 	-- 	local spawnPos = Player.Character.Head.Position + dir * 5;
+			-- 	-- 	-- args[1] = spawnPos;
+			-- 	-- 	-- args[2] = dir;
+			-- 	-- end
+			-- end
 
 
 			return oldNameCall(self, unpack(args))
