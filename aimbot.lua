5c83dcfa128fb5b319dd7ecda381b82f0f9b042c
bowlimon <bowlimon@protonmail.com>
2022-07-31 22:27:33 +0100

    More testing

--- aimbot.lua
+++ aimbot.lua
@@ -267,13 +267,13 @@ local function main()
 			end
 
 			if not checkcaller() and self == updateEvent and namecallMethod == "FireServer" then
-				local data = settings.targetPlayer and playerData[settings.targetPlayer];
-				if data then
-					local dir = getDir(settings.targetPlayer, data.newPos, data.moveDirection, data.walkSpeed)
-					local spawnPos = Player.Character.Head.Position + dir * 5;
-					-- args[1] = spawnPos;
-					-- args[2] = dir;
-				end
+				-- local data = settings.targetPlayer and playerData[settings.targetPlayer];
+				-- if data then
+				-- 	local dir = getDir(settings.targetPlayer, data.newPos, data.moveDirection, data.walkSpeed)
+				-- 	local spawnPos = Player.Character.Head.Position + dir * 5;
+				-- 	-- args[1] = spawnPos;
+				-- 	-- args[2] = dir;
+				-- end
 			end
 
 
